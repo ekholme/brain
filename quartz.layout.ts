fffb9f1c935283a4c34b2cf0083c8c998fca3eb2
Eric Ekholm <eric.ekholm@gmail.com>
2024-11-18 16:20:45 -0500

    recent notes only on desktop

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -27,7 +27,7 @@ export const defaultContentPageLayout: PageLayout = {
     Component.MobileOnly(Component.Spacer()),
     Component.Search(),
     Component.Darkmode(),
-    Component.RecentNotes(),
+    Component.DesktopOnly(Component.RecentNotes()),
     Component.DesktopOnly(Component.Explorer()),
   ],
   right: [
